d6b1a6f8693831e75a9f10c04a1c59e100e2b736
freelife11 <mirrol33@gmail.com>
2025-03-21 14:54:18 +0900

    03.21 update4
    이미지 경로 테스트

--- my-outfront/src/App.js
+++ my-outfront/src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={`${process.env.PUBLIC_URL}/images/1.png`} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
